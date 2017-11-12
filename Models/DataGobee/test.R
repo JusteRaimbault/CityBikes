d571d1f904bcf646191566f03e047300a4375242
Juste Raimbault <juste.raimbault@polytechnique.edu>
2017-11-12 20:56:42 +0100

    .

--- Models/DataGobee/test.R
+++ Models/DataGobee/test.R
@@ -15,16 +15,21 @@ box=bbox(paris)
 lngmin=box[1,1];lngmax=box[1,2]
 latmin=box[2,1];latmax=box[2,2]
 
-plot(paris)
+#plot(paris)
 
-resolution = 8
+resolutions=seq(4,20)
+resolutions=sample(resolutions,size = length(resolutions),replace = F)
+
+bikenum=c();glngmin=c();glatmin=c();glngmax=c();glatmax=c()
+for(resolution in resolutions){
 
 lngstep = (lngmax-lngmin)/resolution
 latstep = (latmax-latmin)/resolution
 
-allids = c()
-for(lat in seq(latmin-(latmax-latmin)/4,latmax+(latmax-latmin)/4,latstep)){
-  for(lng in seq(lngmin- (lngmax-lngmin)/4,lngmax+ (lngmax-lngmin)/4,lngstep)){
+allids = c();alllats=c();alllngs=c()
+for(lat in seq(latmin-(latmax-latmin)/2,latmax+(latmax-latmin)/2,latstep)){
+  for(lng in seq(lngmin- (lngmax-lngmin)/2,lngmax+ (lngmax-lngmin)/2,lngstep)){
+    show(paste0(lng,' - ',lat))
     dat = content(GET(paste0("http://aws.gobee.bike/GobeeBike/bikes/near_bikes?lat=",lat,"&lng=",lng,"&accuracy=20")),type="application/json")
     bikes = dat[["data"]][["bikes"]]
 
@@ -33,14 +38,26 @@ for(lat in seq(latmin-(latmax-latmin)/4,latmax+(latmax-latmin)/4,latstep)){
     ids = sapply(bikes,function(l){l$number})
     
     allids = unique(c(allids,ids))
+    alllats = unique(c(alllats,lats))
+    alllngs = unique(c(alllngs,lngs))
     
-    if(length(lats)>0&length(lngs)>0){
-      points = SpatialPoints(matrix(data=c(lngs,lats),nrow = length(lats),byrow = F),proj4string = paris@proj4string)
-      plot(points,col='red',add=T)
-    }
+    #if(length(lats)>0&length(lngs)>0){
+    #  points = SpatialPoints(matrix(data=c(lngs,lats),nrow = length(lats),byrow = F),proj4string = paris@proj4string)
+    #  plot(points,col='red',add=T)
+    #}
   }
 }
+bikenum=append(bikenum,length(allids));
+glngmin=append(glngmin,min(alllngs));
+glngmax=append(glngmax,max(alllngs));
+glatmin=append(glatmin,min(alllats));
+glatmax=append(glatmax,max(alllats));
 
+}
+
+res=data.frame(bikenum,glngmin,glngmax,glatmin,glatmax,resolutions)
+
+save(res,file='resol.RData')
 
 
 
